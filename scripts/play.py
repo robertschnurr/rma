42dbfd04f60e1d995e7cfa112287304948c4383a
Robert Schnurr <rschnurr123@hotmail.com>
2026-04-02 17:07:32 -0500

    change video name

--- scripts/play.py
+++ scripts/play.py
@@ -142,8 +142,11 @@ def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agen
 
     # wrap for video recording
     if args_cli.video:
+        from datetime import datetime
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
         video_kwargs = {
             "video_folder": os.path.join(log_dir, "videos", "play"),
+            "name_prefix": timestamp,
             "step_trigger": lambda step: step == 0,
             "video_length": args_cli.video_length,
             "disable_logger": True,
